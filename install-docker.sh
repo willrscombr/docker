db1f666829d119ea3e918ffe69f3eb69e63bd38c
dev <wilton.tecti@gmail.com>
2017-04-27 15:02:08 -0300

    instalador configurado e funcionando

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,10 @@
+# Faz o dawnload do script na url mencinada e executa via sh
 wget -qO- https://get.docker.com/ | sh
-docker container run hello-world
-curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose\
-2 -`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-3 chmod +x /usr/local/bin/docker-compose
+# Executa um container de testes
+docker container run hello-world&
+# Faz o download do docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose\-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+# Pemiote executar de execução
+chmod +x /usr/local/bin/docker-compose
+# Verifica versão
 docker-compose version
