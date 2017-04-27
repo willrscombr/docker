1e547a1205a8124b855d8948479c9ba9da44755c
dev <wilton.tecti@gmail.com>
2017-04-27 15:15:12 -0300

    Corrigido erro na url do docker compose

--- install-docker.sh
+++ install-docker.sh
@@ -1,9 +1,9 @@
 # Faz o dawnload do script na url mencinada e executa via sh
-wget -qO- https://get.docker.com/ | sh
+#wget -qO- https://get.docker.com/ | sh
 # Executa um container de testes
 docker container run hello-world&
 # Faz o download do docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose\-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 # Pemiote executar de execução
 chmod +x /usr/local/bin/docker-compose
 # Verifica versão
