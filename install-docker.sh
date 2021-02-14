fbd98a8bb2aefd5f265dee64876d47c21f014306
willtecti <wilton.tecti@gmail.com>
2021-02-14 20:08:34 -0300

    Upgrade docker-compose 1.28.2

--- install-docker.sh
+++ install-docker.sh
@@ -3,7 +3,7 @@ wget -qO- https://get.docker.com/ | sh
 # Executa um container de testes
 docker container run hello-world&
 # Faz o download do docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.28.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 # Pemiote executar de execução
 chmod +x /usr/local/bin/docker-compose
 # Verifica versão
